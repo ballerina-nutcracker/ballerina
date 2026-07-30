7ee7dd0d3dca80c4674a7d4896ce09554c9fa27e
Heshan Padamsiri <hpheshan@gmail.com>
2026-07-30 07:40:11 +0530

    fix(ast): remove trailing whitespaces in ast pretty printer

--- ast/pretty_printer.go
+++ ast/pretty_printer.go
@@ -349,7 +349,9 @@ func (p *PrettyPrinter) printImportPackage(node *BLangImportPackage) {
 func (p *PrettyPrinter) printCompilationUnit(node *BLangCompilationUnit) {
 	p.StartNode()
 	p.PrintString("compilation-unit")
-	p.PrintString(node.Name)
+	if node.Name != "" {
+		p.PrintString(node.Name)
+	}
 	p.printPackageID(node.packageID)
 	p.printBLangNodeBase(&node.bLangNodeBase)
 	p.indentLevel++
